71fe961f02fc474f047c851d7fafb4b235c0cf3c
dimitraye <dimitraye94@gmail.com>
2022-08-13 16:53:58 +0200

    first draft

---
+++ models/thing.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+
+
+
+
+userSchema.plugin(uniqueValidator);
+
+
+module.exports = mongoose.model('Thing', thingSchema);
+
+module.exports = mongoose.model('User', userSchema);
